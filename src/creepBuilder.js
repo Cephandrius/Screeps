3e73a812b122c01bc72a972cf7956b2bc8f6b45b
Cephandrius <collegechristopherschuckjr@hotmail.com>
2016-10-07 22:29:31 -0600

    Update

--- src/creepBuilder.js
+++ src/creepBuilder.js
@@ -8,5 +8,9 @@ Methods:
   harvest (steal from harvester)
   fill(steal from carrier)
 */
+var creepBase = require("creepBase");
 
+function creepBuilder(creepId){//I don't know how constructors are inherited
+  new creepBase.call(this,creepId);
+}
 
