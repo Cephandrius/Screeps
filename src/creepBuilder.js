59ebac6ab31e8958809646d930f6ed1ea8f19ac3
Cephandrius <collegechristopherschuckjr@hotmail.com>
2016-10-07 23:39:33 -0600

    Update creepBuilder.js

--- src/creepBuilder.js
+++ src/creepBuilder.js
@@ -1,10 +1,5 @@
 /*Implementation list:
-Vars:
-  repairThingsYouPassBy
 Methods:
-  repair
-  repairAction
-  harvest (steal from harvester)
   fill(steal from carrier)
 */
 var creepBase = require("creepBase");
@@ -26,47 +21,98 @@ return: whether or not the creep is full
 notes: this is a priavte function, don't use
 */
 creepBuilder.gatherEnergy = function(){
-  var energyNeeded = this.creep.carryCapacity-this.creep.carry[RESOURCE_ENERGY];
-  var energyAvailible = this.secondaryTarget.energy;
-  if(energyAvailible == undefined){
-    energyAvailible = this.secondaryTarget.store[RESOURCE_ENERGY];
-    if(energyAvailible === undefined){
-      this.creep.harvest(this.secondaryTarget);
-      if(this.creep.carry[RESOURCE_ENERGY]==this.creep.carryCapacity){
-        return true;
+  var result = this.creep.withdraw(this.secondaryTarget,RESOURCE_ENERGY);
+  if(result == ERR_INVALID_TARGET){
+    this.creep.harvest(this.secondaryTarget);
+    if(this.creep.carry[RESOURCE_ENERGY]==this.creep.carryCapacity){
+      return true;
+    }else{
+      return false;
+    }
+  }else if(result!=OK){
+    console.log("There was an error in the build action of creep (", this.creep.pos.x,",",this.creep.pos.y,") with an error code ",result);
+    return false;
+  }else{
+    return true;
+  }
+}
+
+/*
+args: nothing
+action: will run the build action for the builder
+returns: nothing
+notes: none
+*/
+creepBuilder.buildAction = function(){
+  var result = this.move();
+  if(result){
+    if(this.fillingEnergy){
+       result = this.gatherEnergy();
+       if(result){
+          this.fillingEnergy = false;
+          var end = this.primaryTarget.pos;
+          end.x = end.x-1;
+          var result = this.room.findPath(this.creep.pos,end,
+                                          {ignoreCreeps:true,serialize:true})
+          this.movePath = result;
+        }
+    }else{
+      if(this.creep.energy == 0){
+        this.fillingEnergy = true;
+        var result = this.room.findPath(this.creep.pos,this.secondaryTarget.pos,
+                                        {ignoreCreeps:true,serialize:true})
+        this.movePath = result;
       }else{
-        return false;
+        this.creep.build(this.primaryTarget);
+        if((this.primaryTarget.progressTotal-this.primaryTarget.progress) == 0){
+          this.room.addIdleCreep(this.id);
+        }
       }
     }
   }
-  energyNeeded = (energyNeeded>energyAvailible)?energyAvailible:energyNeeded;
-  result = this.creep.withdraw(this.secondaryTarget,RESOURCE_ENERGY,energyNeeded);
-  if(result!=OK){
-    console.log("There was an error in the build action of creep (", this.creep.pos.x,",",this.creep.pos.y,") with an error code ",result);
-    return false;
-  }
-  return true;
 }
-creepBuilder.buildAction = function(){
-  this.fillingEnergy = false;
+
+creepBuilder.repairAction = function(){
+  var result = this.move();
+  if(result){
+    if(this.fillingEnergy){
+      result = this.gatherEnergy();
+      if(result){
+        this.fillingEnergy = false;
         var end = this.primaryTarget.pos;
         end.x = end.x-1;
         var result = this.room.findPath(this.creep.pos,end,
                                         {ignoreCreeps:true,serialize:true})
         this.movePath = result;
-  var result = this.move();
-  if(result){
-    if(this.fillingEnergy){
-      
-   }else{
-    if(this.creep.energy == 0){
-      this.fillingEnergy = true;
-      var result = this.room.findPath(this.creep.pos,this.secondaryTarget.pos,
-                                      {ignoreCreeps:true,serialize:true})
-      this.movePath = result;
-    }else{
-     this.creep.build(this.primaryTarget);
+      }
+    }else{//This needs to be modified
+      this.creep.repair(this.primaryTarget);
+      if(this.primaryTarget.hits==this.primaryTarget.hitsMax){
+        this.room.addIdleCreep(this.id);
+      }
     }
   }
 }
 
+creepBuilder.fill = function(){
+   var result = this.move();
+  if(result){
+    if(this.fillingEnergy){
+      result = this.gatherEnergy();
+      if(result){
+        this.fillingEnergy = false;
+        var end = this.primaryTarget.pos;
+        end.x = end.x-1;
+        var result = this.room.findPath(this.creep.pos,end,
+                                        {ignoreCreeps:true,serialize:true})
+        this.movePath = result;
+      }
+    }else{//This needs to be modified
+      this.creep.transfer(this.primaryTarget,RESOURCE_ENERGY);
+      this.room.addIdleCreep(this.id);
+      }
+    }
+  }
+}
+
+module.exports = creepBuilder;
