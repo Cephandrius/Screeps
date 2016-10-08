85370ae42256f0cf6588cb1a68980301e0d2338d
Cephandrius <collegechristopherschuckjr@hotmail.com>
2016-10-07 19:50:02 -0600

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,1 +1,8 @@
-//This should be a comment
+/*This is the harvester role. What the creep will do is sit on top of a container and extract energy from source or extractor and dump it 
+into the container.Some of them may be given defense capabilities*/
+
+
+//Constructor
+function Harvester(creep){
+  
+}
