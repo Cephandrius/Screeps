796adca7a2d43f2d206182eb92a544931af58cfd
Cephandrius <collegechristopherschuckjr@hotmail.com>
2016-10-08 01:33:48 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+var room = require("room");
+var creepBase = require("creepBase");
+var creepBuilder = require("creepBuilder");
 
+module.exports.loop = function () {
+  var rooms = [];
+  for(aRoom in Game.rooms){
+    if(aRoom.memory.initalized != undefined){
+      rooms.push(new room(aRoom.id));
+    }  
+  }
+  for(int i = 0;i<rooms.length;i++){
+    rooms[i].assesSituation(); 
+  }
+  for(int i = 0;i<rooms.length;i++){
+    rooms[i].orderCreeps(); 
+  }
+  for(int i = 0;i<rooms.length;i++){
+    rooms[i].saveMemory(); 
+  }
+}
